51d68728fd74d76464a082b3ebed1db85848a5bd
ronitahuja <93635738+ronitahuja@users.noreply.github.com>
2023-03-29 10:09:05 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-quest.form.get('contactname'),
-                                        request.form.get('branch').upper(),
-                                        request.form.get('contactemail'),
-                                        request.form.get('contactmsg'),
-                                        })
